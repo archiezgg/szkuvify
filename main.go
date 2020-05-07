3af197810910ad87e9b8aaec9a70ef93b297598f
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-05-08 00:58:39 +0200

    init(main): add main func and szkuvihandler

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	token       = os.Getenv("SZKUVI_TOKEN")
+	baseMessage = "rozsul montot te ozstoba kecifei. hejesen: "
+)
+
+var szkuviRules = map[rune]rune{
+	'v': 'f',
+	'g': 'k',
+	'b': 'p',
+	'd': 't',
+	'j': 'i',
+}
+
+var yRules = map[rune]rune{
+	'g': 't',
+	'l': 'j',
+}
+
+func main() {
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Println("error creating Discord session: ", err)
+		return
+	}
+	defer discord.Close()
+
+	discord.AddHandler(szkuviHandler)
+
+	err = discord.Open()
+	if err != nil {
+		log.Println("error opening Discord session: ", err)
+	}
+
+	log.Println("szkuvify is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+}
+
+func szkuviHandler(discord *discordgo.Session, message *discordgo.MessageCreate) {
+	if message.Author.Bot {
+		return
+	}
+	m := baseMessage + szkuvify(message.Content)
+	discord.ChannelMessageSend(message.ChannelID, m)
+}
+
+func szkuvify(text string) string {
+	var szkuvifiedPhrase string
+
+	for i, letter := range text {
+		specialLetter, isSpecial := yRules[letter]
+		if isSpecial && i != len(text)-1 && text[i+1] == 'y' {
+			szkuvifiedPhrase += string(specialLetter)
+			continue
+		}
+
+		szkuviLetter, isRule := szkuviRules[letter]
+		if isRule {
+			szkuvifiedPhrase += string(szkuviLetter)
+		} else {
+			szkuvifiedPhrase += string(letter)
+		}
+	}
+	return szkuvifiedPhrase
+}
